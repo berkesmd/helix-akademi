aff04f5c63850fc3f34c4a940bb81e7d26a5785f
berkesmd <besadin1940@gmail.com>
2026-08-03 00:24:43 +0300

    PWA ayarlari tamamlandi

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,24 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
+
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+
+  typescript:{
+    ignoreBuildErrors:true,
   },
+
 };
 
-export default nextConfig;
+
+export default withPWA({
+
+  dest:"public",
+
+  register:true,
+
+  workboxOptions:{
+    skipWaiting:true,
+  },
+
+})(nextConfig);
