4278dfee8f33ef36a908a4fa9de6cdf2ab9c0167
berkesmd <besadin1940@gmail.com>
2026-08-03 00:32:20 +0300

    pwa turbopack fix

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,10 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors:true,
   },
 
+  turbopack:{},
+
 };
+
 
 
 export default withPWA({
@@ -20,5 +23,6 @@ export default withPWA({
   workboxOptions:{
     skipWaiting:true,
   },
+
 
 })(nextConfig);
